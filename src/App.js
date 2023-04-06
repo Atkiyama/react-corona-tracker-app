ec48ec1fd1968d963c13b25f3d7641d24fb5bede
Atkiyama <thisisatkiyama@gmail.com>
2023-04-06 17:34:45 +0900

    Result.js-1まで完成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import TopPage from "./pages/Toppage";
 import './App.css';
+import countriesJson from "./countries.json"
+import {useState} from "react";
 
 
 function App() {
+
+  const [country,setCountry] = useState("");
+  const getCountryData = () => {
+      fetch('https://monotein-books.vercel.app/api/corona-tracker/country/${country}')
+      .then(res => res.json())
+      .then(data =>console.log(data))
+  }
   return (
     <div>
      
-      <TopPage />
+      <TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData}/>
     </div>
   );
 }
