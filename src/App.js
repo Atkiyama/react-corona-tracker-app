06757b835d547602a317235de5dfcc2159ca6b29
Atkiyama <thisisatkiyama@gmail.com>
2023-04-06 17:09:38 +0900

    selector完成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import TopPage from "./pages/Toppage";
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+     
+      <TopPage />
     </div>
   );
 }
